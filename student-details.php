b21a4295364c596dc8da3b44af96a0a17d8e93ba
Raica Kathleen Perez <146512506+raicsp@users.noreply.github.com>
2024-11-13 10:23:17 +0800

    Update student-details.php

--- student-details.php
+++ student-details.php
@@ -98,7 +98,7 @@ $conn->close();
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>Administrator | Laboratory School | Batangas State University TNEU</title>
+  <title>Laboratory School | Batangas State University TNEU</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
   <!-- Favicons -->
