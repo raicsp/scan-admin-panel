dfdbe99f5cfa22a2ab40b4a2ee16d99638e82fad
Raica Kathleen Perez <146512506+raicsp@users.noreply.github.com>
2024-12-09 21:22:05 +0800

    Update teacher-report.php

--- teacher-report.php
+++ teacher-report.php
@@ -310,7 +310,7 @@ $activePage = 'attendance-report';
             // Close loading spinner after delay
             setTimeout(() => {
               Swal.close();
-            }, 8000); // Simulated delay for generation
+            }, 4000); // Simulated delay for generation
           }
         });
       }
@@ -352,7 +352,7 @@ $activePage = 'attendance-report';
           // Close loading spinner after delay
           setTimeout(() => {
             Swal.close();
-          }, 8000); // Simulated delay for generation
+          }, 4000); // Simulated delay for generation
         }
       });
     });
